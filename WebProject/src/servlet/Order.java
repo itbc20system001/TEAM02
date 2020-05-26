a520a1ede8a6dfca0a3579b50dcf52f31323b5df
aya-oda <aya-oda@NA20426.nature.local>
2020-05-26 16:00:25 +0900

    model:CreditCard 追加 クレジットカード番号表示用スコープの作成

--- WebProject/src/servlet/Order.java
+++ WebProject/src/servlet/Order.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.CreditCard;
+
 @WebServlet("/Order")
 public class Order extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -20,6 +22,16 @@ public class Order extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		//リクエストパラメータ
+		request.setCharacterEncoding("UTF-8");
+		String creditCard = request.getParameter("creditCard");
+
+
+		//リクエストスコープ
+		CreditCard cardNumber = new CreditCard(creditCard);
+		request.setAttribute("creditCard", cardNumber);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Order.jsp");
 		dispatcher.forward(request, response);
 

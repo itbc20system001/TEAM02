72d500f66c4a88d0cf37726938f5f9d4d234ab88
ueura <ueura@NA19492.nature.local>
2020-05-28 11:13:54 +0900

    5/28 カートに情報をセッションスコープで保存、取得

--- WebProject/src/servlet/CartServlet.java
+++ WebProject/src/servlet/CartServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Cart;
 
 
 @WebServlet("/Cart")
@@ -23,15 +26,43 @@ public class CartServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-				String hook = request.getParameter("hook");
-				System.out.println(hook);
-
-						RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Cart.jsp");
-						dispatcher.forward(request, response);
 
 
 	}
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	int hook = Integer.parseInt(request.getParameter("hook"));
+		int cloth = Integer.parseInt(request.getParameter("cloth"));
+		int size = Integer.parseInt(request.getParameter("size"));
+		int quantity = Integer.parseInt(request.getParameter("quantity"));
+		int  pattern = Integer.parseInt(request.getParameter("pattern"));
+		int price = 1000;
+
+		System.out.println(quantity);
+
+		boolean hook_flg;
+		if(hook==0){
+		hook_flg=true;
+		}else{
+		hook_flg=false;
+		}
+
+		boolean cloth_flg;
+		if(hook==0){
+		cloth_flg=true;
+		}else{
+		cloth_flg=false;
+		}
+
+
+
+		Cart c = new Cart(pattern, size, cloth_flg, hook_flg, quantity, price);
+		HttpSession session = request.getSession();
+		session.setAttribute("Cart", c);
+
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Cart.jsp");
+				dispatcher.forward(request, response);
+
 
 
 
